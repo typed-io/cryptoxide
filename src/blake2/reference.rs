1867405bb4e06f1609b16e00ea1b321d3e675193
Vincent Hanquez <vincent@typed.io>
2020-03-04 22:45:42 +0000

    flatten sigma and rounds

--- src/blake2/reference.rs
+++ src/blake2/reference.rs
@@ -9,21 +9,6 @@ mod b {
     pub(super) const R3: u32 = 16;
     pub(super) const R4: u32 = 63;
 
-    pub(super) const SIGMA: [[usize; 16]; 12] = [
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-        [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-        [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-        [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-        [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
-        [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
-        [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
-        [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
-        [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-    ];
-
     pub(super) const IV: [u64; 8] = [
         0x6a09e667f3bcc908,
         0xbb67ae8584caa73b,
@@ -47,19 +32,6 @@ mod s {
     pub(super) const R3: u32 = 8;
     pub(super) const R4: u32 = 7;
 
-    pub(super) const SIGMA: [[usize; 16]; 10] = [
-        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
-        [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
-        [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
-        [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
-        [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
-        [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
-        [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
-        [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
-        [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
-        [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
-    ];
-
     pub(super) const IV: [u32; 8] = [
         0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB,
         0x5BE0CD19,
@@ -67,6 +39,24 @@ mod s {
 
     pub(super) const ROUNDS: usize = 10;
 }
+
+// SIGMA is the same for the b and s variant. except that
+// in the B variant, there's a 11th and 12th row that is copy of
+// the 1st and 2nd.
+pub(super) const SIGMA: [[usize; 16]; 12] = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+    [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+    [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+    [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+    [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
+    [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
+    [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
+    [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
+    [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+];
 
 #[derive(Clone, PartialEq, Eq)]
 pub enum LastBlock {
@@ -76,15 +66,11 @@ pub enum LastBlock {
 
 macro_rules! G {
     ($conmod:ident, $r:expr, $i:expr, $a:expr, $b:expr, $c:expr, $d:expr, $m:expr) => {
-        $a = $a
-            .wrapping_add($b)
-            .wrapping_add($m[$conmod::SIGMA[$r][2 * $i + 0]]);
+        $a = $a.wrapping_add($b).wrapping_add($m[SIGMA[$r][2 * $i + 0]]);
         $d = ($d ^ $a).rotate_right($conmod::R1);
         $c = $c.wrapping_add($d);
         $b = ($b ^ $c).rotate_right($conmod::R2);
-        $a = $a
-            .wrapping_add($b)
-            .wrapping_add($m[$conmod::SIGMA[$r][2 * $i + 1]]);
+        $a = $a.wrapping_add($b).wrapping_add($m[SIGMA[$r][2 * $i + 1]]);
         $d = ($d ^ $a).rotate_right($conmod::R3);
         $c = $c.wrapping_add($d);
         $b = ($b ^ $c).rotate_right($conmod::R4);
@@ -120,8 +106,19 @@ macro_rules! compressbody {
             vs[14] = !vs[14];
         }
 
-        for i in 0..$conmod::ROUNDS {
-            round!($conmod, i, vs, ms);
+        round!($conmod, 0, vs, ms);
+        round!($conmod, 1, vs, ms);
+        round!($conmod, 2, vs, ms);
+        round!($conmod, 3, vs, ms);
+        round!($conmod, 4, vs, ms);
+        round!($conmod, 5, vs, ms);
+        round!($conmod, 6, vs, ms);
+        round!($conmod, 7, vs, ms);
+        round!($conmod, 8, vs, ms);
+        round!($conmod, 9, vs, ms);
+        if $conmod::ROUNDS == 12 {
+            round!($conmod, 10, vs, ms);
+            round!($conmod, 11, vs, ms);
         }
 
         for (h_elem, (v_low, v_high)) in $engine

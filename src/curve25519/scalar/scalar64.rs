fc1280a6e155aa524e52d3c615adc82c15da03b0
Vincent Hanquez <vincent@typed.io>
2022-01-07 13:53:14 +0800

    rewrite muladd as mul + add

--- src/curve25519/scalar/scalar64.rs
+++ src/curve25519/scalar/scalar64.rs
@@ -56,7 +56,7 @@ const MASK40: u64 = 0x0000_00ff_ffff_ffff;
 const MASK56: u64 = 0x00ff_ffff_ffff_ffff;
 
 #[rustfmt::skip]
-fn barrett_reduce256_modm(out: &mut [u64; 5], q1: &[u64; 5], r1: [u64; 5]) {
+fn barrett_reduce256_modm(out: &mut [u64; 5], q1: &[u64; 5], r1: &[u64; 5]) {
     let mut r2 = [0; 5];
     let mut q3 = [0; 5];
     let mut c : u128;
@@ -278,128 +278,16 @@ Output:
     where l = 2^252 + 27742317777372353535851937790883648493.
 */
 #[rustfmt::skip]
-pub(crate) fn muladd(s: &mut[u8; 32], a: &[u8; 32], b: &[u8; 32], c: &[u8; 32]) {
-    let a0 = 2097151 & load_3i(&a[0..3]);
-    let a1 = 2097151 & (load_4i(&a[2..6]) >> 5);
-    let a2 = 2097151 & (load_3i(&a[5..8]) >> 2);
-    let a3 = 2097151 & (load_4i(&a[7..11]) >> 7);
-    let a4 = 2097151 & (load_4i(&a[10..14]) >> 4);
-    let a5 = 2097151 & (load_3i(&a[13..16]) >> 1);
-    let a6 = 2097151 & (load_4i(&a[15..19]) >> 6);
-    let a7 = 2097151 & (load_3i(&a[18..21]) >> 3);
-    let a8 = 2097151 & load_3i(&a[21..24]);
-    let a9 = 2097151 & (load_4i(&a[23..27]) >> 5);
-    let a10 = 2097151 & (load_3i(&a[26..29]) >> 2);
-    let a11 = load_4i(&a[28..32]) >> 7;
-    let b0 = 2097151 & load_3i(&b[0..3]);
-    let b1 = 2097151 & (load_4i(&b[2..6]) >> 5);
-    let b2 = 2097151 & (load_3i(&b[5..8]) >> 2);
-    let b3 = 2097151 & (load_4i(&b[7..11]) >> 7);
-    let b4 = 2097151 & (load_4i(&b[10..14]) >> 4);
-    let b5 = 2097151 & (load_3i(&b[13..16]) >> 1);
-    let b6 = 2097151 & (load_4i(&b[15..19]) >> 6);
-    let b7 = 2097151 & (load_3i(&b[18..21]) >> 3);
-    let b8 = 2097151 & load_3i(&b[21..24]);
-    let b9 = 2097151 & (load_4i(&b[23..27]) >> 5);
-    let b10 = 2097151 & (load_3i(&b[26..29]) >> 2);
-    let b11 = load_4i(&b[28..32]) >> 7;
-    let c0 = 2097151 & load_3i(&c[0..3]);
-    let c1 = 2097151 & (load_4i(&c[2..6]) >> 5);
-    let c2 = 2097151 & (load_3i(&c[5..8]) >> 2);
-    let c3 = 2097151 & (load_4i(&c[7..11]) >> 7);
-    let c4 = 2097151 & (load_4i(&c[10..14]) >> 4);
-    let c5 = 2097151 & (load_3i(&c[13..16]) >> 1);
-    let c6 = 2097151 & (load_4i(&c[15..19]) >> 6);
-    let c7 = 2097151 & (load_3i(&c[18..21]) >> 3);
-    let c8 = 2097151 & load_3i(&c[21..24]);
-    let c9 = 2097151 & (load_4i(&c[23..27]) >> 5);
-    let c10 = 2097151 & (load_3i(&c[26..29]) >> 2);
-    let c11 = load_4i(&c[28..32]) >> 7;
-    let mut s0: i64;
-    let mut s1: i64;
-    let mut s2: i64;
-    let mut s3: i64;
-    let mut s4: i64;
-    let mut s5: i64;
-    let mut s6: i64;
-    let mut s7: i64;
-    let mut s8: i64;
-    let mut s9: i64;
-    let mut s10: i64;
-    let mut s11: i64;
-    let mut s12: i64;
-    let mut s13: i64;
-    let mut s14: i64;
-    let mut s15: i64;
-    let mut s16: i64;
-    let mut s17: i64;
-    let mut s18: i64;
-    let mut s19: i64;
-    let mut s20: i64;
-    let mut s21: i64;
-    let mut s22: i64;
-    let mut s23: i64;
-    let mut carry0: i64;
-    let mut carry1: i64;
-    let mut carry2: i64;
-    let mut carry3: i64;
-    let mut carry4: i64;
-    let mut carry5: i64;
-    let mut carry6: i64;
-    let mut carry7: i64;
-    let mut carry8: i64;
-    let mut carry9: i64;
-    let mut carry10: i64;
-    let mut carry11: i64;
-    let mut carry12: i64;
-    let mut carry13: i64;
-    let mut carry14: i64;
-    let mut carry15: i64;
-    let mut carry16: i64;
-    let carry17: i64;
-    let carry18: i64;
-    let carry19: i64;
-    let carry20: i64;
-    let carry21: i64;
-    let carry22: i64;
+pub(crate) fn muladd(s: &mut [u8; 32], a: &[u8; 32], b: &[u8; 32], c: &[u8; 32]) {
+    let a = from_bytes(a);
+    let b = from_bytes(b);
+    let c = from_bytes(c);
 
-    s0 = c0 + a0*b0;
-    s1 = c1 + a0*b1 + a1*b0;
-    s2 = c2 + a0*b2 + a1*b1 + a2*b0;
-    s3 = c3 + a0*b3 + a1*b2 + a2*b1 + a3*b0;
-    s4 = c4 + a0*b4 + a1*b3 + a2*b2 + a3*b1 + a4*b0;
-    s5 = c5 + a0*b5 + a1*b4 + a2*b3 + a3*b2 + a4*b1 + a5*b0;
-    s6 = c6 + a0*b6 + a1*b5 + a2*b4 + a3*b3 + a4*b2 + a5*b1 + a6*b0;
-    s7 = c7 + a0*b7 + a1*b6 + a2*b5 + a3*b4 + a4*b3 + a5*b2 + a6*b1 + a7*b0;
-    s8 = c8 + a0*b8 + a1*b7 + a2*b6 + a3*b5 + a4*b4 + a5*b3 + a6*b2 + a7*b1 + a8*b0;
-    s9 = c9 + a0*b9 + a1*b8 + a2*b7 + a3*b6 + a4*b5 + a5*b4 + a6*b3 + a7*b2 + a8*b1 + a9*b0;
-    s10 = c10 + a0*b10 + a1*b9 + a2*b8 + a3*b7 + a4*b6 + a5*b5 + a6*b4 + a7*b3 + a8*b2 + a9*b1 + a10*b0;
-    s11 = c11 + a0*b11 + a1*b10 + a2*b9 + a3*b8 + a4*b7 + a5*b6 + a6*b5 + a7*b4 + a8*b3 + a9*b2 + a10*b1 + a11*b0;
-    s12 = a1*b11 + a2*b10 + a3*b9 + a4*b8 + a5*b7 + a6*b6 + a7*b5 + a8*b4 + a9*b3 + a10*b2 + a11*b1;
-    s13 = a2*b11 + a3*b10 + a4*b9 + a5*b8 + a6*b7 + a7*b6 + a8*b5 + a9*b4 + a10*b3 + a11*b2;
-    s14 = a3*b11 + a4*b10 + a5*b9 + a6*b8 + a7*b7 + a8*b6 + a9*b5 + a10*b4 + a11*b3;
-    s15 = a4*b11 + a5*b10 + a6*b9 + a7*b8 + a8*b7 + a9*b6 + a10*b5 + a11*b4;
-    s16 = a5*b11 + a6*b10 + a7*b9 + a8*b8 + a9*b7 + a10*b6 + a11*b5;
-    s17 = a6*b11 + a7*b10 + a8*b9 + a9*b8 + a10*b7 + a11*b6;
-    s18 = a7*b11 + a8*b10 + a9*b9 + a10*b8 + a11*b7;
-    s19 = a8*b11 + a9*b10 + a10*b9 + a11*b8;
-    s20 = a9*b11 + a10*b10 + a11*b9;
-    s21 = a10*b11 + a11*b10;
-    s22 = a11*b11;
-    s23 = 0;
+    let m = mul(&a, &b);
+    let r = add(&m, &c);
 
-    carry0 = (s0 + (1<<20)) >> 21; s1 += carry0; s0 -= carry0 << 21;
-    carry2 = (s2 + (1<<20)) >> 21; s3 += carry2; s2 -= carry2 << 21;
-    carry4 = (s4 + (1<<20)) >> 21; s5 += carry4; s4 -= carry4 << 21;
-    carry6 = (s6 + (1<<20)) >> 21; s7 += carry6; s6 -= carry6 << 21;
-    carry8 = (s8 + (1<<20)) >> 21; s9 += carry8; s8 -= carry8 << 21;
-    carry10 = (s10 + (1<<20)) >> 21; s11 += carry10; s10 -= carry10 << 21;
-    carry12 = (s12 + (1<<20)) >> 21; s13 += carry12; s12 -= carry12 << 21;
-    carry14 = (s14 + (1<<20)) >> 21; s15 += carry14; s14 -= carry14 << 21;
-    carry16 = (s16 + (1<<20)) >> 21; s17 += carry16; s16 -= carry16 << 21;
-    carry18 = (s18 + (1<<20)) >> 21; s19 += carry18; s18 -= carry18 << 21;
-    carry20 = (s20 + (1<<20)) >> 21; s21 += carry20; s20 -= carry20 << 21;
-    carry22 = (s22 + (1<<20)) >> 21; s23 += carry22; s22 -= carry22 << 21;
+    *s = to_bytes(&r);
+}
 
 #[cfg(test)]
 mod tests {

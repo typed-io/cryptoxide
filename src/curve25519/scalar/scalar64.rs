1348ac649813fdfeb406081e2557baebf9bb44ed
Vincent Hanquez <vincent@typed.io>
2022-01-07 13:49:18 +0800

    add tests

--- src/curve25519/scalar/scalar64.rs
+++ src/curve25519/scalar/scalar64.rs
@@ -312,219 +312,69 @@ pub(crate) fn muladd(s: &mut[u8; 32], a: &[u8; 32], b: &[u8; 32], c: &[u8; 32]) 
     carry20 = (s20 + (1<<20)) >> 21; s21 += carry20; s20 -= carry20 << 21;
     carry22 = (s22 + (1<<20)) >> 21; s23 += carry22; s22 -= carry22 << 21;
 
-    carry1 = (s1 + (1<<20)) >> 21; s2 += carry1; s1 -= carry1 << 21;
-    carry3 = (s3 + (1<<20)) >> 21; s4 += carry3; s3 -= carry3 << 21;
-    carry5 = (s5 + (1<<20)) >> 21; s6 += carry5; s5 -= carry5 << 21;
-    carry7 = (s7 + (1<<20)) >> 21; s8 += carry7; s7 -= carry7 << 21;
-    carry9 = (s9 + (1<<20)) >> 21; s10 += carry9; s9 -= carry9 << 21;
-    carry11 = (s11 + (1<<20)) >> 21; s12 += carry11; s11 -= carry11 << 21;
-    carry13 = (s13 + (1<<20)) >> 21; s14 += carry13; s13 -= carry13 << 21;
-    carry15 = (s15 + (1<<20)) >> 21; s16 += carry15; s15 -= carry15 << 21;
-    carry17 = (s17 + (1<<20)) >> 21; s18 += carry17; s17 -= carry17 << 21;
-    carry19 = (s19 + (1<<20)) >> 21; s20 += carry19; s19 -= carry19 << 21;
-    carry21 = (s21 + (1<<20)) >> 21; s22 += carry21; s21 -= carry21 << 21;
-
-    s11 += s23 * 666643;
-    s12 += s23 * 470296;
-    s13 += s23 * 654183;
-    s14 -= s23 * 997805;
-    s15 += s23 * 136657;
-    s16 -= s23 * 683901;
-
-
-    s10 += s22 * 666643;
-    s11 += s22 * 470296;
-    s12 += s22 * 654183;
-    s13 -= s22 * 997805;
-    s14 += s22 * 136657;
-    s15 -= s22 * 683901;
-
-
-    s9 += s21 * 666643;
-    s10 += s21 * 470296;
-    s11 += s21 * 654183;
-    s12 -= s21 * 997805;
-    s13 += s21 * 136657;
-    s14 -= s21 * 683901;
-
-
-    s8 += s20 * 666643;
-    s9 += s20 * 470296;
-    s10 += s20 * 654183;
-    s11 -= s20 * 997805;
-    s12 += s20 * 136657;
-    s13 -= s20 * 683901;
-
-
-    s7 += s19 * 666643;
-    s8 += s19 * 470296;
-    s9 += s19 * 654183;
-    s10 -= s19 * 997805;
-    s11 += s19 * 136657;
-    s12 -= s19 * 683901;
-
-
-    s6 += s18 * 666643;
-    s7 += s18 * 470296;
-    s8 += s18 * 654183;
-    s9 -= s18 * 997805;
-    s10 += s18 * 136657;
-    s11 -= s18 * 683901;
-
-
-    carry6 = (s6 + (1<<20)) >> 21; s7 += carry6; s6 -= carry6 << 21;
-    carry8 = (s8 + (1<<20)) >> 21; s9 += carry8; s8 -= carry8 << 21;
-    carry10 = (s10 + (1<<20)) >> 21; s11 += carry10; s10 -= carry10 << 21;
-    carry12 = (s12 + (1<<20)) >> 21; s13 += carry12; s12 -= carry12 << 21;
-    carry14 = (s14 + (1<<20)) >> 21; s15 += carry14; s14 -= carry14 << 21;
-    carry16 = (s16 + (1<<20)) >> 21; s17 += carry16; s16 -= carry16 << 21;
-
-    carry7 = (s7 + (1<<20)) >> 21; s8 += carry7; s7 -= carry7 << 21;
-    carry9 = (s9 + (1<<20)) >> 21; s10 += carry9; s9 -= carry9 << 21;
-    carry11 = (s11 + (1<<20)) >> 21; s12 += carry11; s11 -= carry11 << 21;
-    carry13 = (s13 + (1<<20)) >> 21; s14 += carry13; s13 -= carry13 << 21;
-    carry15 = (s15 + (1<<20)) >> 21; s16 += carry15; s15 -= carry15 << 21;
-
-    s5 += s17 * 666643;
-    s6 += s17 * 470296;
-    s7 += s17 * 654183;
-    s8 -= s17 * 997805;
-    s9 += s17 * 136657;
-    s10 -= s17 * 683901;
-
-
-    s4 += s16 * 666643;
-    s5 += s16 * 470296;
-    s6 += s16 * 654183;
-    s7 -= s16 * 997805;
-    s8 += s16 * 136657;
-    s9 -= s16 * 683901;
-
-
-    s3 += s15 * 666643;
-    s4 += s15 * 470296;
-    s5 += s15 * 654183;
-    s6 -= s15 * 997805;
-    s7 += s15 * 136657;
-    s8 -= s15 * 683901;
-
-
-    s2 += s14 * 666643;
-    s3 += s14 * 470296;
-    s4 += s14 * 654183;
-    s5 -= s14 * 997805;
-    s6 += s14 * 136657;
-    s7 -= s14 * 683901;
-
-
-    s1 += s13 * 666643;
-    s2 += s13 * 470296;
-    s3 += s13 * 654183;
-    s4 -= s13 * 997805;
-    s5 += s13 * 136657;
-    s6 -= s13 * 683901;
-
-
-    s0 += s12 * 666643;
-    s1 += s12 * 470296;
-    s2 += s12 * 654183;
-    s3 -= s12 * 997805;
-    s4 += s12 * 136657;
-    s5 -= s12 * 683901;
-    s12 = 0;
-
-    carry0 = (s0 + (1<<20)) >> 21; s1 += carry0; s0 -= carry0 << 21;
-    carry2 = (s2 + (1<<20)) >> 21; s3 += carry2; s2 -= carry2 << 21;
-    carry4 = (s4 + (1<<20)) >> 21; s5 += carry4; s4 -= carry4 << 21;
-    carry6 = (s6 + (1<<20)) >> 21; s7 += carry6; s6 -= carry6 << 21;
-    carry8 = (s8 + (1<<20)) >> 21; s9 += carry8; s8 -= carry8 << 21;
-    carry10 = (s10 + (1<<20)) >> 21; s11 += carry10; s10 -= carry10 << 21;
-
-    carry1 = (s1 + (1<<20)) >> 21; s2 += carry1; s1 -= carry1 << 21;
-    carry3 = (s3 + (1<<20)) >> 21; s4 += carry3; s3 -= carry3 << 21;
-    carry5 = (s5 + (1<<20)) >> 21; s6 += carry5; s5 -= carry5 << 21;
-    carry7 = (s7 + (1<<20)) >> 21; s8 += carry7; s7 -= carry7 << 21;
-    carry9 = (s9 + (1<<20)) >> 21; s10 += carry9; s9 -= carry9 << 21;
-    carry11 = (s11 + (1<<20)) >> 21; s12 += carry11; s11 -= carry11 << 21;
-
-    s0 += s12 * 666643;
-    s1 += s12 * 470296;
-    s2 += s12 * 654183;
-    s3 -= s12 * 997805;
-    s4 += s12 * 136657;
-    s5 -= s12 * 683901;
-    s12 = 0;
-
-    carry0 = s0 >> 21; s1 += carry0; s0 -= carry0 << 21;
-    carry1 = s1 >> 21; s2 += carry1; s1 -= carry1 << 21;
-    carry2 = s2 >> 21; s3 += carry2; s2 -= carry2 << 21;
-    carry3 = s3 >> 21; s4 += carry3; s3 -= carry3 << 21;
-    carry4 = s4 >> 21; s5 += carry4; s4 -= carry4 << 21;
-    carry5 = s5 >> 21; s6 += carry5; s5 -= carry5 << 21;
-    carry6 = s6 >> 21; s7 += carry6; s6 -= carry6 << 21;
-    carry7 = s7 >> 21; s8 += carry7; s7 -= carry7 << 21;
-    carry8 = s8 >> 21; s9 += carry8; s8 -= carry8 << 21;
-    carry9 = s9 >> 21; s10 += carry9; s9 -= carry9 << 21;
-    carry10 = s10 >> 21; s11 += carry10; s10 -= carry10 << 21;
-    carry11 = s11 >> 21; s12 += carry11; s11 -= carry11 << 21;
-
-    s0 += s12 * 666643;
-    s1 += s12 * 470296;
-    s2 += s12 * 654183;
-    s3 -= s12 * 997805;
-    s4 += s12 * 136657;
-    s5 -= s12 * 683901;
-
-
-    carry0 = s0 >> 21; s1 += carry0; s0 -= carry0 << 21;
-    carry1 = s1 >> 21; s2 += carry1; s1 -= carry1 << 21;
-    carry2 = s2 >> 21; s3 += carry2; s2 -= carry2 << 21;
-    carry3 = s3 >> 21; s4 += carry3; s3 -= carry3 << 21;
-    carry4 = s4 >> 21; s5 += carry4; s4 -= carry4 << 21;
-    carry5 = s5 >> 21; s6 += carry5; s5 -= carry5 << 21;
-    carry6 = s6 >> 21; s7 += carry6; s6 -= carry6 << 21;
-    carry7 = s7 >> 21; s8 += carry7; s7 -= carry7 << 21;
-    carry8 = s8 >> 21; s9 += carry8; s8 -= carry8 << 21;
-    carry9 = s9 >> 21; s10 += carry9; s9 -= carry9 << 21;
-    carry10 = s10 >> 21; s11 += carry10; s10 -= carry10 << 21;
-
-    s[0] = (s0 >> 0) as u8;
-    s[1] = (s0 >> 8) as u8;
-    s[2] = ((s0 >> 16) | (s1 << 5)) as u8;
-    s[3] = (s1 >> 3) as u8;
-    s[4] = (s1 >> 11) as u8;
-    s[5] = ((s1 >> 19) | (s2 << 2)) as u8;
-    s[6] = (s2 >> 6) as u8;
-    s[7] = ((s2 >> 14) | (s3 << 7)) as u8;
-    s[8] = (s3 >> 1) as u8;
-    s[9] = (s3 >> 9) as u8;
-    s[10] = ((s3 >> 17) | (s4 << 4)) as u8;
-    s[11] = (s4 >> 4) as u8;
-    s[12] = (s4 >> 12) as u8;
-    s[13] = ((s4 >> 20) | (s5 << 1)) as u8;
-    s[14] = (s5 >> 7) as u8;
-    s[15] = ((s5 >> 15) | (s6 << 6)) as u8;
-    s[16] = (s6 >> 2) as u8;
-    s[17] = (s6 >> 10) as u8;
-    s[18] = ((s6 >> 18) | (s7 << 3)) as u8;
-    s[19] = (s7 >> 5) as u8;
-    s[20] = (s7 >> 13) as u8;
-    s[21] = (s8 >> 0) as u8;
-    s[22] = (s8 >> 8) as u8;
-    s[23] = ((s8 >> 16) | (s9 << 5)) as u8;
-    s[24] = (s9 >> 3) as u8;
-    s[25] = (s9 >> 11) as u8;
-    s[26] = ((s9 >> 19) | (s10 << 2)) as u8;
-    s[27] = (s10 >> 6) as u8;
-    s[28] = ((s10 >> 14) | (s11 << 7)) as u8;
-    s[29] = (s11 >> 1) as u8;
-    s[30] = (s11 >> 9) as u8;
-    s[31] = (s11 >> 17) as u8;
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::curve25519::testrng::{GeneratorOf, GeneratorOf2, GeneratorRaw};
+
+    fn next_scalar(gen: &mut GeneratorRaw) -> [u64; 5] {
+        let mut bytes = gen.bytes();
+        bytes[31] &= 0x0f; // 2^252 max for simplicity
+        from_bytes(&bytes)
+    }
+
+    #[test]
+    fn serialization() {
+        for scalar in GeneratorOf::new(0, 100, next_scalar) {
+            let after_serialization = from_bytes(&to_bytes(&scalar));
+            assert_eq!(scalar, after_serialization);
+        }
+    }
+
+    #[test]
+    fn add_iv() {
+        struct Iv {
+            a: [u8; 32],
+            b: [u8; 32],
+            r: [u8; 32],
+        }
+
+        let ivs = [Iv {
+            a: [
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0,
+            ],
+            b: [
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+            ],
+            r: [
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 15, 16, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0,
+            ],
+        }];
+
+        for (i, iv) in ivs.iter().enumerate() {
+            let a = from_bytes(&iv.a);
+            let b = from_bytes(&iv.b);
+            let r = add(&a, &b);
+            assert_eq!(to_bytes(&r), iv.r, "iv test {} failed", i);
+        }
+    }
+
+    #[test]
+    fn add_commutes() {
+        for (x, y) in GeneratorOf2::new(0, 100, next_scalar) {
+            assert_eq!(add(&x, &y), add(&y, &x));
+        }
+    }
+
+    #[test]
+    fn mul_commutes() {
+        for (x, y) in GeneratorOf2::new(0, 100, next_scalar) {
+            assert_eq!(mul(&x, &y), mul(&y, &x));
+        }
+    }
 
     #[test]
     fn reduction() {

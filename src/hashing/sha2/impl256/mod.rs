0a5781547d632a3a07a8daf8d78dc31ee3c9162e
Evgenii Lisitskii <lisicky@inbox.ru>
2023-02-15 08:07:48 +0400

    Compilation for x86 x86_64 without AVX (#39)
    * fix avx preprocessor checks
    
    * fmt

--- src/hashing/sha2/impl256/mod.rs
+++ src/hashing/sha2/impl256/mod.rs
@@ -10,9 +10,17 @@
 
 #[cfg(all(target_arch = "aarch64", feature = "use-stdsimd"))]
 mod aarch64;
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "avx"
+))]
 mod avx;
-#[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
+
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "sse4.1"
+))]
 mod sse41;
 //TODO not finished yet
 //#[cfg(all(target_arch = "x86_64", target_feature = "sha"))]
@@ -35,11 +43,18 @@ pub(crate) fn digest_block(state: &mut [u32; 8], block: &[u8]) {
         #[cfg(not(target_feature = "sse4.1"))]
         const HAS_SSE41: bool = false;
 
-        if HAS_AVX {
-            return avx::digest_block(state, block);
+        #[cfg(target_feature = "avx")]
+        {
+            if HAS_AVX {
+                return avx::digest_block(state, block);
+            }
         }
-        if HAS_SSE41 {
-            return sse41::digest_block(state, block);
+
+        #[cfg(target_feature = "sse4.1")]
+        {
+            if HAS_SSE41 {
+                return sse41::digest_block(state, block);
+            }
         }
     }
     #[cfg(target_arch = "aarch64")]
